56df596535e1072e9a0a87a1703e481c00257218
Aziz <ngwk.zz@outlook.com>
2024-03-28 00:17:14 +0300

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
     """
     def __init__(self) -> None:
         self.__redis = redis.Redis()
-        self.__redis.flushdb(True)
+        self.__redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
@@ -22,29 +22,3 @@ class Cache:
         rand_key = str(uuid.uuid4())
         self.__redis.set(rand_key, data)
         return rand_key
-
-    def get(self, key: str, fn: Callable = None) -> Union[str,
-                                                          bytes, int, None]:
-        """
-        take a key string argument and an optional Callable argument named fn
-        """
-        data = self.__redis.get(key)
-        if data is None:
-            return None
-        if fn is not None:
-            return fn(data)
-        return data
-
-    def get_str(sel, key: str) -> Union[str, None]:
-        """
-        automatically parametrize Cache.get with the correc
-        conversion function.
-        """
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
-
-    def get_int(self, key: str) -> Union[int, None]:
-        """
-        automatically parametrize Cache.get with the correct
-        conversion function.
-        """
-        return self.get(key, fn=int)
