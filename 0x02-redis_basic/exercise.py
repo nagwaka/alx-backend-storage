984374d180629a187f20d23b9adade6154bf8d01
Aziz <ngwk.zz@outlook.com>
2024-03-28 00:54:19 +0300

    Read from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,13 +12,13 @@ class Cache:
     cache class for storing data into Redis db
     """
     def __init__(self) -> None:
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store the input data in Redis using the random key and return the key.
         """
         rand_key = str(uuid.uuid4())
-        self.__redis.set(rand_key, data)
+        self._redis.set(rand_key, data)
         return rand_key
