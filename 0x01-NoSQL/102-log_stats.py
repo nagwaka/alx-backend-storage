26cf3e309d9acd3ba24df2fcdc66352fc6aaac5a
Aziz <ngwk.zz@outlook.com>
2024-03-29 17:58:45 +0300

    Log stats - new version

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Script that provides some stats about Nginx logs stored in MongoDB
+Improve 12-log_stats.py by adding the top 10 of the most present IPs
+in the collection nginx of the database logs
 """
 from pymongo import MongoClient
 
@@ -19,7 +20,14 @@ def get_logs_stats(mongo_collection):
     status_count = mongo_collection.count_documents(
                     {"method": "GET", "path": "/status"})
 
-    return total_logs, method_counts, status_count
+    # Get top 10 most present IPs
+    top_ips = mongo_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    return total_logs, method_counts, status_count, top_ips
 
 
 if __name__ == "__main__":
@@ -28,7 +36,8 @@ if __name__ == "__main__":
     collection = db["nginx"]
 
     # Get stats
-    total_logs, method_counts, status_count = get_logs_stats(collection)
+    total_logs, method_counts, status_count, top_ips = get_logs_stats(
+                                                                    collection)
 
     # Display stats
     print(f"{total_logs} logs")
@@ -36,3 +45,7 @@ if __name__ == "__main__":
     for method, count in method_counts.items():
         print(f"\tmethod {method}: {count}")
     print(f"{status_count} status check")
+
+    print("IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
