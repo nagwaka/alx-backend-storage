18fc32535a1b8a59596974cb83f2a082167561cf
Aziz <ngwk.zz@outlook.com>
2024-03-29 17:42:16 +0300

    Return all students sorted by average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Function that returns all students sorted by average score
+"""
+
+def top_students(mongo_collection):
+    """
+    Return all students sorted by average score
+    """
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
